5f45546dc1b32b115eb20484c1af8f26cff541fb
Abdur Rahman Adil <abdurrahmanadil005@gmail.com>
2025-06-14 15:56:52 +0600

    style change

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ document.getElementById("cal-btn").addEventListener("click", () => {
         alert("Please enter a valid birthday");
         return;
     }
-
     let birthDate = new Date(userInput);
-
     if (birthDate > today) {
         alert("You haven't been born yet!");
         return;
     }
-
     let todayYear = today.getFullYear();
     let todayMonth = today.getMonth() + 1;
     let todayDay = today.getDate();
@@ -27,16 +24,22 @@ document.getElementById("cal-btn").addEventListener("click", () => {
 
     if (resultDay < 0) {
         resultMonth--;
-        const daysInPrevMonth = new Date(todayYear, todayMonth - 1, 0).getDate();
+        const daysInPrevMonth = 
+        new Date(todayYear, todayMonth - 1, 0).getDate();
         resultDay += daysInPrevMonth;
     }
-
     if (resultMonth < 0) {
         resultYear--;
         resultMonth += 12;
     }
-
     document.getElementById("year-in").innerHTML = resultYear;
     document.getElementById("month-in").innerHTML = resultMonth;
     document.getElementById("day-in").innerHTML = resultDay;
+    document.getElementById("rst-btn").addEventListener("click",()=>{
+        document.getElementById("dob-in").value="";
+         document.getElementById("year-in").innerHTML = "0";
+    document.getElementById("month-in").innerHTML = "0";
+    document.getElementById("day-in").innerHTML = "0";
+    });
+    
 });
